fb23091479fd415fbea1a06951b304f168ac7a7c
Thi Vu <thi.vu@tuni.fi>
2023-09-01 22:02:24 +0300

    Mean4

--- round2/mean/src/main/java/mean/Mean.java
+++ round2/mean/src/main/java/mean/Mean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mean;
+package mean;
 
 /**
  *
@@ -34,3 +34,4 @@ public class Mean {
         } 
     }
 }
+
