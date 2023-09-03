6087c999d5863045e08fa6a8c9e1f7f2c70e7994
Thi Vu <thi.vu@tuni.fi>
2023-09-03 16:53:08 +0300

    mean8

--- round2/mean/src/main/java/com/mycompany/mean/Mean.java
+++ round2/mean/src/main/java/com/mycompany/mean/Mean.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 public class Mean {
 
     public static void main(String[] args) {
-        System.out.println("Enter numbers: ");
+        System.out.print("Enter numbers: ");
         try (Scanner myScanner = new Scanner(System.in)) {
             String line = myScanner.nextLine();
             
@@ -30,7 +30,7 @@ public class Mean {
             }
             double mean = sum/count;
             
-            System.out.println("Mean: " + mean);
+            System.out.print("Mean: " + mean);
         } 
     }
 }
