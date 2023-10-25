c0417edd001cdccb71e3f548f77a85b2b2ebdfd6
Thi Vu <thi.vu@tuni.fi>
2023-10-26 01:04:21 +0300

    Streams5

--- round8/streams/src/main/java/fi/tuni/prog3/streams/MovieAnalytics.java
+++ round8/streams/src/main/java/fi/tuni/prog3/streams/MovieAnalytics.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
@@ -52,19 +53,23 @@ public class MovieAnalytics {
     }
 
     public Stream<Movie> moviesAfter(int year){
-        return movies.stream().filter(movie -> movie.getReleaseYear() >= year).sorted();
+        return movies.stream().filter(movie -> movie.getReleaseYear() >= year)
+        .sorted(Comparator.comparing(Movie::getReleaseYear)).sorted(Comparator.comparing(Movie::getTitle));
     }
 
     public Stream<Movie> moviesBefore(int year){
-        return movies.stream().filter(movie -> movie.getReleaseYear() <= year).sorted();
+        return movies.stream().filter(movie -> movie.getReleaseYear() <= year)
+        .sorted(Comparator.comparing(Movie::getReleaseYear)).sorted(Comparator.comparing(Movie::getTitle));
     }
 
     public Stream<Movie> moviesBetween(int yearA, int yearB){
-        return movies.stream().filter(movie -> movie.getReleaseYear() >= yearA && movie.getReleaseYear() <= yearB).sorted();
+        return movies.stream().filter(movie -> movie.getReleaseYear() >= yearA && movie.getReleaseYear() <= yearB)
+        .sorted(Comparator.comparing(Movie::getReleaseYear)).sorted(Comparator.comparing(Movie::getTitle));
     }
 
     public Stream<Movie> moviesByDirector(String director){
-        return movies.stream().filter(movie -> movie.getDirector().equals(director)).sorted();
+        return movies.stream().filter(movie -> movie.getDirector().equals(director)).sorted()
+        .sorted(Comparator.comparing(Movie::getReleaseYear)).sorted(Comparator.comparing(Movie::getTitle));
     }
 
 }
