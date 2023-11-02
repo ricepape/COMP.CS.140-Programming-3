6a857f8218572700a412d34b2387c842170f696f
Thi Vu <thi.vu@tuni.fi>
2023-11-02 23:10:32 +0200

    javadoc11

--- round10/javadocjson/src/main/java/fi/tuni/prog3/json/Node.java
+++ round10/javadocjson/src/main/java/fi/tuni/prog3/json/Node.java
@@ -5,15 +5,27 @@ package fi.tuni.prog3.json;
  */
 public abstract class Node {
 
+  /**
+   * Print the JSON representation of the node to the standard output.
+   */
+  public void printJson() {
+    StringBuilder sb = new StringBuilder();
+    printJson(this, sb, 0);
+    System.out.println(sb.toString());
+  }
+
+  private static final String NL = System.lineSeparator();
+
+  private static String numberToString(Double d) {
+    String str = Double.toString(d);
+    if(str.endsWith(".0")) {
+      str = str.substring(0, str.length() - 2);
+    }
+    return str;
+  }
+
   private static final String CNL = "," + System.lineSeparator();
 
-  /**
-   * Prints a JSON representation of the provided Node and its children recursively.
-   *
-   * @param node   The Node to be printed.
-   * @param sb     The StringBuilder used for building the JSON string.
-   * @param depth  The current depth of the recursion.
-   */
   private void printJson(Node node, StringBuilder sb, int depth) {
     final String indent = "  ".repeat(depth);
     final String subIndent = "  ".repeat(depth + 1);
@@ -68,10 +80,9 @@ public abstract class Node {
       sb.append(valStr);
     }
   }
-
+  
   /**
    * Check if the node is an array node.
-   *
    * @return true if the node is an array node, otherwise false.
    */
   public boolean isArray() {
@@ -80,7 +91,6 @@ public abstract class Node {
 
   /**
    * Check if the node is an object node.
-   *
    * @return true if the node is an object node, otherwise false.
    */
   public boolean isObject() {
@@ -89,30 +99,10 @@ public abstract class Node {
 
   /**
    * Check if the node is a value node.
-   *
    * @return true if the node is a value node, otherwise false.
    */
   public boolean isValue() {
     return this instanceof ValueNode;
-  }
-
-  /**
-   * Print the JSON representation of the node to the standard output.
-   */
-  public void printJson() {
-    StringBuilder sb = new StringBuilder();
-    printJson(this, sb, 0);
-    System.out.println(sb.toString());
-  }
-
-  private static final String NL = System.lineSeparator();
-
-  private static String numberToString(Double d) {
-    String str = Double.toString(d);
-    if(str.endsWith(".0")) {
-      str = str.substring(0, str.length() - 2);
-    }
-    return str;
   }
 
 }
