7e870faa1d66b5a4388acbee0a4e6e6441cda400
Thi Vu <thi.vu@tuni.fi>
2023-11-10 18:04:31 +0200

    junitorder1

---
+++ round11/junitorder/src/main/java/fi/tuni/prog3/junitorder/Order.java
@@ -0,0 +1,168 @@
+package fi.tuni.prog3.junitorder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class Order {
+    public static class Entry {
+        private Item item;
+        private int count;
+
+        public Entry(Item item, int count) throws IllegalArgumentException {
+            if (count <= 0) {
+                throw new IllegalArgumentException("Item unit count must be positive");
+            }
+            this.item = item;
+            this.count = count;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        // Method to get the item
+        public Item getItem() {
+            return item;
+        }
+
+        public void removeUnits(int des_count) {
+            count -= des_count;
+        }
+
+        // Method to create a string representation of the entry
+        @Override
+        public String toString() {
+            return count + " units of " + item.toString();
+        }
+    }
+
+    public static class Item {
+        private final String name;
+        private final double unitPrice;
+
+        // Constructor for Item
+        public Item(String name, double unitPrice) throws IllegalArgumentException {
+            if (name == null || unitPrice < 0) {
+                throw new IllegalArgumentException("Invalid input: name cannot be null, and price cannot be negative.");
+            }
+            this.name = name;
+            this.unitPrice = unitPrice;
+        }
+
+        // Getter method for name
+        public String getName() {
+            return name;
+        }
+
+        // Getter method for unit price
+        public double getUnitPrice() {
+            return unitPrice;
+        }
+
+        // toString method for Item
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    private List<Entry> entries;
+
+    // Constructor
+    public Order() {
+        this.entries = new ArrayList<>();
+    }
+
+    // Method to add items to the order based on Order.Item
+    public boolean addItems(Item item, int count) throws IllegalArgumentException {
+        // Adds count units of an item to the order.
+        // Note: Implement the logic based on the provided documentation.
+        // If needed, handle IllegalArgumentException.
+        if (count <= 0) {
+            throw new IllegalArgumentException("Item unit count must be positive");
+        }
+
+        Entry entry = new Entry(item, count);
+        entries.add(entry);
+        return true;
+    }
+
+    // Method to add items to the order based on item name
+    public boolean addItems(String name, int count) throws IllegalArgumentException, NoSuchElementException {
+        // Adds count units of an item to the order based on item name.
+        // Note: Implement the logic based on the provided documentation.
+        // If needed, handle IllegalArgumentException and NoSuchElementException.
+        if (count <= 0) {
+            throw new IllegalArgumentException("Item unit count must be positive");
+        }
+
+        Item item = new Item(name, 0.0); // You may need to provide a unit price here
+        Entry entry = new Entry(item, count);
+        entries.add(entry);
+        return true;
+    }
+
+    // Method to get order entries
+    public List<Entry> getEntries() {
+        // Returns the order entries in their original adding order.
+        // Note: The returned list should be a copy to avoid modifying the internal state.
+        return new ArrayList<>(entries);
+    }
+
+    // Method to get the total number of item entries in the order
+    public int getEntryCount() {
+        // Returns the total number of item entries in this order.
+        // Note: Implement the logic based on the provided documentation.
+        return entries.size();
+    }
+
+    // Method to get the total number of entries in the order
+    public int getItemCount() {
+        // Returns the total number of entries in this order (= sum of all entries' counts).
+        // Note: Implement the logic based on the provided documentation.
+        int totalCount = 0;
+        for (Entry entry : entries) {
+            totalCount += entry.getCount();
+        }
+        return totalCount;
+    }
+
+    // Method to get the total price of the order
+    public double getTotalPrice() {
+        double totalPrice = 0.0;
+        for (Entry entry : entries) {
+            totalPrice += entry.getCount() * entry.getItem().getUnitPrice();
+        }
+        return totalPrice;
+    }
+
+    // Method to check if the order is empty
+    public boolean isEmpty() {
+        // Tells whether the order is empty.
+        // Note: Implement the logic based on the provided documentation.
+        return entries.isEmpty();
+    }
+
+    // Method to remove items from the order
+    public boolean removeItems(String name, int count) throws IllegalArgumentException {
+        // Removes count units of an item from the order.
+        // Note: Implement the logic based on the provided documentation.
+        // If needed, handle IllegalArgumentException.
+        if (count <= 0) {
+            throw new IllegalArgumentException("Item unit count must be positive");
+        }
+
+        for (Entry entry : entries) {
+            if (entry.getItem().getName().equals(name)) {
+                if (entry.getCount() <= count) {
+                    entries.remove(entry);
+                } else {
+                    entry.removeUnits(count);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ round11/junitorder/src/test/java/fi/tuni/prog3/junitorder/OrderIT.java
@@ -0,0 +1,165 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package fi.tuni.prog3.junitorder;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author vudinhthi2304
+ */
+public class OrderIT {
+    
+    public OrderIT() {
+    }
+
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+    /**
+     * Test of addItems method, of class Order.
+     */
+    @Test
+    public void testAddItems_OrderItem_int() {
+        System.out.println("addItems");
+        Order.Item item = new Order.Item("Item1", 10.0);
+        int count = 0;
+        Order instance = new Order();
+        boolean expResult = true;
+        boolean result = instance.addItems(item, count);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addItems method, of class Order.
+     */
+    @Test
+    public void testAddItems_String_int() {
+        System.out.println("addItems");
+        String name = "milk";
+        int count = 0;
+        Order instance = new Order();
+        boolean expResult = true;
+        boolean result = instance.addItems(name, count);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEntries method, of class Order.
+     */
+    @Test
+    public void testGetEntries() {
+        System.out.println("getEntries");
+        Order instance = new Order();
+        List<Order.Entry> expResult = null;
+        List<Order.Entry> result = instance.getEntries();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEntryCount method, of class Order.
+     */
+    @Test
+    public void testGetEntryCount() {
+        System.out.println("getEntryCount");
+        Order instance = new Order();
+        int expResult = 0;
+        int result = instance.getEntryCount();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getItemCount method, of class Order.
+     */
+    @Test
+    public void testGetItemCount() {
+        System.out.println("getItemCount");
+        Order instance = new Order();
+        int expResult = 0;
+        int result = instance.getItemCount();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTotalPrice method, of class Order.
+     */
+    @Test
+    public void testGetTotalPrice() {
+        System.out.println("getTotalPrice");
+        Order instance = new Order();
+        double expResult = 0.0;
+        double result = instance.getTotalPrice();
+        assertEquals(expResult, result, 0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isEmpty method, of class Order.
+     */
+    @Test
+    public void testEmptyOrder() {
+        Order order = new Order();
+
+        assertTrue(order.isEmpty());
+        assertEquals(0, order.getEntryCount());
+        assertEquals(0, order.getItemCount());
+        assertEquals(0.0, order.getTotalPrice(), 0.001);
+    }
+
+    /**
+     * Test of removeItems method, of class Order.
+     */
+    @Test
+    public void testRemoveItems() {
+        System.out.println("removeItems");
+        String name = "";
+        int count = 0;
+        Order instance = new Order();
+        boolean expResult = false;
+        boolean result = instance.removeItems(name, count);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testAddItemsWithNegativeCount() {
+        Order order = new Order();
+        Order.Item item = new Order.Item("Item1", 10.0);
+        assertThrows(IllegalArgumentException.class, () ->  order.addItems(item, -5));
+    }   
+    
+
+}
